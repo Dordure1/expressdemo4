edeafa3d9fe56bd20924cda42c870564c97df3c1
Dorian <dorian.vanhoutte1@gmail.com>
2022-11-03 16:42:30 +0100

    templating

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 9000
 
+const lead = {firstName : "Dorian", age : 26}
+
+const devs = [
+    {firstName : "Pignouf1", age :33},
+    {firstName : "Pignouf2", age :33},
+    {firstName : "Pignouf3", age :33}
+]
 
 //// Config du moteur de vue
 //- Quel moteur utiliser  !!Moteur de vue doit etre installé!! 
@@ -10,10 +17,16 @@ app.set('view engine','ejs')
 app.set("views","views")
 
 
-
 /// Routing 
 app.get('/', (req,res)=>{
-    res.send("<h1>Home</h1>")
+    const now = new Date().toLocaleTimeString()
+    const message = "Hello all"
+    res.render('home/index', {now, message})
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('home/about', { lead, devs })
 })
 
 
